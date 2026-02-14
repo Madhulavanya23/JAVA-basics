6b8db5ccea3f98c92f3d852bfa2cbfdb3d7d9a1f
Madhulavanya23 <madhulavanyalavanya621@gmail.com>
2026-02-14 10:27:29 +0530

    Add files via upload

---
+++ EvenorOdd.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class EvenorOdd{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int n=sc.nextInt();
+        if(n%2==0)
+            System.out.println("number is even");
+        else
+            System.out.println("number is odd");
+    }
+}
